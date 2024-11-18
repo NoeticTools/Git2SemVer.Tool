9ed416f8f24b04a329d5000cefa0006d01c096c1
Rob Smyth <RobSmyth@users.noreply.github.com>
2024-11-18 11:07:39 +1100

    Initial commit - moving from Git2SemVer repository

---
+++ Git2SemVer.Tool.Tests/CommandLine/Git2SemVerCommandAppTests.cs
@@ -0,0 +1,20 @@
+﻿using NoeticTools.Git2SemVer.Tool.CommandLine;
+
+
+namespace NoeticTools.Git2SemVer.Tool.Tests.CommandLine;
+
+[TestFixture]
+internal class Git2SemVerCommandAppTests
+{
+    [Test]
+    public void Test()
+    {
+        var target = new Git2SemVerCommandApp();
+
+        //target.Execute(["-h"]);
+
+        //target.Execute(["add", "-s", "xxxx", "-x"]);
+
+        target.Execute(["--version"]);
+    }
+}
